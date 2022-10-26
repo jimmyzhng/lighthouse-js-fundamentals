dd5b87e1bb0e80baa37b9b10f02616c67a946487
Jimmy <jimmyzhang1@hotmail.com>
2022-10-25 22:01:24 -0700

    Banana Bread Ingredients Challenge

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+whileLoopList = function (ingredients) {
+  let i = 0;
+  console.log("Banana Bread Ingredients")
+  while (i <= ingredients.length) {
+    console.log(ingredients[i]);
+    i++;
+  }
+
+whileLoopList(ingredients);
+// Write a for loop that prints out the contents of ingredients:
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
