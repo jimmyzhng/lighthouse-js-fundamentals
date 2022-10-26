365a319f17a20eae0b116ef1bb07456f8d62f79b
Jimmy <jimmyzhang1@hotmail.com>
2022-10-25 21:42:05 -0700

    hello to console

--- hello.js
+++ hello.js
@@ -3,3 +3,25 @@ const sayHello = function (name) {
 }
 
 sayHello("Bob");
+
+//Console.log
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+//Return
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+// Variable that calls a function
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+
+console.log(greeting);
+
